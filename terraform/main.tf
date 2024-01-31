ed7e852616f12243616491dfa9effe86c37055cf
Younes <melloukiy4@gmail.com>
2024-01-31 13:34:47 +0100

    feature terraform-main-variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,46 +2,37 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.51.0"
+      version = "5.6.0"
     }
   }
 }
 
 provider "google" {
-# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
-  #credentials = "./keys/nyc-de-gcp-8e0479ba1c9d.json"
-  project = "nyc-de-gcp-412823"
-  region  = "us-central1"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
 }
 
 
+resource "google_storage_bucket" "demo-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
 
-resource "google_storage_bucket" "data-lake-bucket" {
-  name          = "nyc-de-gcp-2024-01"
-  location      = "US"
-  # Optional, but recommended settings:
-  storage_class = "STANDARD"
-  uniform_bucket_level_access = true
-
-  versioning {
-    enabled     = true
-  }
 
   lifecycle_rule {
-    action {
-      type = "Delete"
-    }
     condition {
-      age = 30  // days
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
     }
   }
-
-  force_destroy = true
 }
 
 
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = "data_NYC_2021123"
-  project    = "nyc-de-gcp-412823"
-  location   = "US"
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
